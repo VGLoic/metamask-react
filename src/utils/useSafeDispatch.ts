45ea6dee9585e705fbe327e34b3c992636166265
Adi Li <adi-li@users.noreply.github.com>
2021-11-10 16:37:14 +0800

    Hide `useLayoutEffect` warning when using SSR
    When using SSR and invoked `useLayoutEffect` inside the component, a warning is prompted.
    
    ```
    Warning: useLayoutEffect does nothing on the server, because its effect cannot be encoded into the server renderer's output format. This will lead to a mismatch between the initial, non-hydrated UI and the intended UI. To avoid this, useLayoutEffect should only be used in components that render exclusively on the client. See https://reactjs.org/link/uselayouteffect-ssr for common fixes.
    ```
    
    Please see the [hack](https://medium.com/@alexandereardon/uselayouteffect-and-ssr-192986cdcf7a) proposed by @alexandereardon.

--- src/utils/useSafeDispatch.ts
+++ src/utils/useSafeDispatch.ts
@@ -1,9 +1,15 @@
 import * as React from "react";
 
+// Hide `useLayoutEffect` warning with SSR
+// See: https://medium.com/@alexandereardon/uselayouteffect-and-ssr-192986cdcf7a
+const useIsomorphicLayoutEffect = typeof window !== 'undefined'
+  ? React.useLayoutEffect
+  : React.useEffect;
+
 export function useSafeDispatch<T>(dispatch: React.Dispatch<T>) {
   const mountedRef = React.useRef(false);
 
-  React.useLayoutEffect(() => {
+  useIsomorphicLayoutEffect(() => {
     mountedRef.current = true;
     return () => {
       mountedRef.current = false;
