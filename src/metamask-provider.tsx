e7b68b7d228d26cfcaeb9a07f90c8f4bc494e43e
VGLoic <vincent.genod.loic@gmail.com>
2023-02-18 18:35:01 +0100

    feat: prioritize MetaMask over Brave Wallet

--- src/metamask-provider.tsx
+++ src/metamask-provider.tsx
@@ -21,13 +21,22 @@ type WindowInstanceWithEthereum = Window &
 function getMetaMaskProvider() {
   const ethereum = (window as WindowInstanceWithEthereum).ethereum;
   if (!ethereum) return null;
-  // The `providers` field is populated when CoinBase Wallet extension is also installed
+  // The `providers` field is populated
+  // - when CoinBase Wallet extension is also installed
+  // - when user is on Brave and Brave Wallet is not deactivated
   // The expected object is an array of providers, the MetaMask provider is inside
   // See https://docs.cloud.coinbase.com/wallet-sdk/docs/injected-provider-guidance for more information
+  // See also https://metamask.zendesk.com/hc/en-us/articles/360038596792-Using-Metamask-wallet-in-Brave-browser
   if (Array.isArray(ethereum.providers)) {
-    const metaMaskProvider = ethereum.providers.find((p: any) => p.isMetaMask);
-    if (!metaMaskProvider) return null;
-    return metaMaskProvider;
+    const metaMaskProvider = ethereum.providers.find(
+      (p: any) => p.isMetaMask && !p.isBraveWallet
+    );
+    if (metaMaskProvider) return metaMaskProvider;
+    const braveWalletProvider = ethereum.providers.find(
+      (p: any) => p.isMetaMask && p.isBraveWallet
+    );
+    if (!braveWalletProvider) return null;
+    return braveWalletProvider;
   }
   if (!ethereum.isMetaMask) return null;
   return ethereum;
