db679645b2bdfcf94c3b7f96d846cac97c50ee97
VGLoic <vincent.genod.loic@gmail.com>
2023-08-15 14:34:44 +0200

    improve documentation of switch chain

--- src/metamask-context.ts
+++ src/metamask-context.ts
@@ -60,7 +60,7 @@ export type IMetaMaskContext = MetaMaskState & {
    * Request addition of a new chain to MetaMask
    * @dev See [MetaMask API](https://docs.metamask.io/wallet/reference/rpc-api/#wallet_addethereumchain) or [EIP-3085](https://eips.ethereum.org/EIPS/eip-3085) for full description
    * @param parameters The chain parameters
-   * @example ```typescript
+   * @example ```ts
    * const { addChain } = useMetaMask();
    * const GNOSIS_MAINNET_PARAMS = {
    *  chainId: "0x64",
@@ -73,16 +73,20 @@ export type IMetaMaskContext = MetaMaskState & {
    *  rpcUrls: ["https://rpc.gnosischain.com/"],
    *  blockExplorerUrls: ["https://gnosisscan.io/"],
    * }
+   * // Add Gnosis chain to MetaMask
    * const onClick = () => addChain(GNOSIS_MAINNET_PARAMS);
    * ```
    */
   addChain: (parameters: AddEthereumChainParameter) => Promise<void>;
   /**
    * Request a switch of network
+   * @dev See [MetaMask API](https://docs.metamask.io/wallet/reference/rpc-api/#wallet_switchethereumchain) or [EIP-3326](https://ethereum-magicians.org/t/eip-3326-wallet-switchethereumchain/5471) for full description
+   * @dev An error with code `4902` will be thrown if the chain has not been added to MetaMask, in this case, use `addChain` first
    * @param chainId Chain ID of the network in hexadecimal
    * @example ```ts
+   * const { addChain } = useMetaMask();
    * // Switch chain to Ethereum Mainnet
-   * await context.switchChain("0x1");
+   * const onClick = () => switchChain("0x1");
    * ```
    */
   switchChain: (chainId: string) => Promise<void>;
